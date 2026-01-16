bc3188e7c9aa22712bcca4dc5a879563f400b523
George Chan <gchan9527@gmail.com>
2026-01-16 15:59:59 +0800

    Refine demosaic code to comply with new ionput/outputs spec
    This way build a cust-graph by functionProto made possible
    
    Signed-off-by: George Chan <gchan9527@gmail.com>

--- onnx/microblocks/demosaic/demosaic_avg_v1.py
+++ onnx/microblocks/demosaic/demosaic_avg_v1.py
@@ -1,4 +1,4 @@
-from microblocks.base import BuildResult
+from microblocks.base import BuildResult, MicroblockBase
 import onnx.helper as oh
 from onnx import TensorProto
 from .demosaic_base import DemosaicBase
@@ -11,7 +11,7 @@ class DemosaicAvgV1(DemosaicBase):
         - input_image [n,4,h,w] (R,G1,G2,B planes)
 
     Provides:
-        - rgb [n,3,h,w] : full-resolution RGB image
+        - applier [n,3,h,w] : full-resolution RGB image
 
     Behavior:
         * Average G1 and G2 -> Gavg
@@ -19,13 +19,21 @@ class DemosaicAvgV1(DemosaicBase):
     """
     name = 'demosaic_avg_v1'
     version = 'v1'
-    needs = ['']
-    provides = ['rgb']
+    needs = ['input_image']
+    provides = ['applier']
 
     def _split_rggb(self, stage, input_image, nodes, vis):
         r, g1, g2, b = [f'{stage}.{ch}' for ch in ('r', 'g1', 'g2', 'b')]
-        nodes.append(oh.make_node('Split', inputs=[input_image], outputs=[r, g1, g2, b], name=f'{stage}.split_rg1g2b', axis=1))
-        return (r, g1, g2, b)
+        nodes.append(
+            oh.make_node(
+                'Split',
+                inputs=[input_image],
+                outputs=[r, g1, g2, b],
+                name=f'{stage}.split_rg1g2b',
+                axis=1
+            )
+        )
+        return r, g1, g2, b
 
     def _avg_green(self, stage, g1, g2, nodes, inits):
         gsum = f'{stage}.g_sum'
@@ -38,7 +46,9 @@ class DemosaicAvgV1(DemosaicBase):
 
     def _concat_rgb(self, stage, r, gavg, b, nodes, vis):
         rgb = f'{stage}.applier'
-        nodes.append(oh.make_node('Concat', inputs=[r, gavg, b], outputs=[rgb], name=f'{stage}.concat_rgb', axis=1))
+        nodes.append(
+            oh.make_node('Concat', inputs=[r, gavg, b], outputs=[rgb], name=f'{stage}.concat_rgb', axis=1)
+        )
         vis.append(oh.make_tensor_value_info(rgb, TensorProto.FLOAT, ['n', 3, 'h', 'w']))
         return rgb
 
@@ -46,11 +56,18 @@ class DemosaicAvgV1(DemosaicBase):
         vis, nodes, inits = ([], [], [])
         upstream = prev_stages[0] if prev_stages else stage
         input_image = f'{upstream}.applier'
+
+        # Build nodes
         r, g1, g2, b = self._split_rggb(stage, input_image, nodes, vis)
         gavg = self._avg_green(stage, g1, g2, nodes, inits)
         applier = self._concat_rgb(stage, r, gavg, b, nodes, vis)
+
         outputs = {'applier': {'name': applier}}
-        return BuildResult(outputs, nodes, inits, vis).appendInput(f'{prev_stages[0]}.applier')
+
+        # BuildResult + declare external input
+        result = BuildResult(outputs, nodes, inits, vis)
+        result.appendInput(input_image)
+        return result
 
     def build_applier(self, stage: str, prev_stages=None):
         return self.build_algo(stage, prev_stages)
