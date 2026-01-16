64c6afb4bbc03c6fbb87d056167f9a32672875b4
George Chan <gchan9527@gmail.com>
2026-01-16 15:41:01 +0800

    Fix blacklevel mb its SSA issue, rewrite for better read
    Signed-off-by: George Chan <gchan9527@gmail.com>

--- onnx/microblocks/blacklevel/blacklevel_base.py
+++ onnx/microblocks/blacklevel/blacklevel_base.py
@@ -1,19 +1,54 @@
-from microblocks.base import BuildResult
+from microblocks.base import BuildResult, MicroblockBase
 import onnx.helper as oh
-from microblocks.base import MicroblockBase
+from onnx import TensorProto
 
 class BlackLevelBase(MicroblockBase):
+    """
+    Black level subtraction microblock.
+    Input:  [N,C,H,W] (image)
+    Output: [N,C,H,W] (image with black level offset removed)
+    Needs:  offset [1] (scalar)
+    """
     name = 'blacklevel'
     version = 'v0'
     deps = ['stride_remove_crop']
     needs = ['offset']
+    provides = ['applier']
 
     def build_applier(self, stage: str, prev_stages=None):
-        out_name = f'{stage}.applier'
-        offset_name = f'{stage}.offset'
         upstream = prev_stages[0] if prev_stages else stage
+
+        # Names
         input_image = f'{upstream}.applier'
-        node = oh.make_node('Sub', inputs=[input_image, offset_name], outputs=[out_name], name=f'{stage}_sub')
-        vis = [oh.make_tensor_value_info(input_image, oh.TensorProto.FLOAT, ['N', 'C', 'H', 'W']), oh.make_tensor_value_info(offset_name, oh.TensorProto.FLOAT, [1]), oh.make_tensor_value_info(out_name, oh.TensorProto.FLOAT, ['N', 'C', 'H', 'W'])]
+        offset_name = f'{stage}.offset'
+        out_name    = f'{stage}.applier'
+
+        # Node
+        sub_node = oh.make_node(
+            'Sub',
+            inputs=[input_image, offset_name],
+            outputs=[out_name],
+            name=f'{stage}_sub'
+        )
+
+        # ValueInfos
+        vis = [
+            oh.make_tensor_value_info(input_image, TensorProto.FLOAT, ['N', 'C', 'H', 'W']),
+            oh.make_tensor_value_info(offset_name, TensorProto.FLOAT, [1]),
+            oh.make_tensor_value_info(out_name,    TensorProto.FLOAT, ['N', 'C', 'H', 'W']),
+        ]
+
         outputs = {'applier': {'name': out_name}}
-        return BuildResult(outputs, [node], [], vis).appendInput(f'{prev_stages[0]}.applier')
+
+        # BuildResult + declare all external needs as inputs
+        result = BuildResult(outputs, [sub_node], [], vis)
+        result.appendInput(input_image)
+        result.appendInput(offset_name)
+        return result
+
+    def build_algo(self, stage: str, prev_stages=None):
+        """
+        Algo path mirrors applier for consistency.
+        Coordinator supplies offset.
+        """
+        return self.build_applier(stage, prev_stages=prev_stages)
